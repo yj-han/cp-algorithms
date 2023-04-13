1eeefba4553063009c1c283edecdf0ce20698442
Yeji Han <phoebe.yeji@gmail.com>
2023-04-13 22:14:17 +0900

    Added segment tree - with/without lazy propagation

---
+++ data-structure/segment_tree/persistent.cpp
@@ -0,0 +1,34 @@
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <bitset>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <numeric>
+#include <cstring>
+
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<bool> vb;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+ 
+#define mp make_pair 
+#define pb push_back
+#define all(v) v.begin(), v.end()
+ 
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+// const int MOD = 998244353;
+
