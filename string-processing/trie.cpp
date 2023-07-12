1aa8010675d40412cdd8da0f8140b5738f595c9b
Yeji Han <phoebe.yeji@gmail.com>
2023-07-12 17:55:54 +0900

    Implemented trie.cpp

---
+++ string-processing/trie.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int K = 26;
+struct Vertex {
+    int next[K];
+    bool output = false;
+
+    Vertex() {
+        fill(begin(next), end(next), -1);
+    }
+};
+
+vector<Vertex> trie(1);
+
+void add_string(string const &s) {
+    int v = 0;
+
+    for (char ch : s) {
+        int c = ch - 'a';
+        if (trie[v].next[c] == -1) {
+            trie[v].next[c] = trie.size();
+            trie.emplace_back();
+        }
+        v = trie[v].next[c];
+    }
+    trie[v].output = true;
+}
