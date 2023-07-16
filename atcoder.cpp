3dc3d766c01a1ec092525b09415db54bc44227ec
Yeji Han <yejicse@gmail.com>
2023-07-16 20:09:28 +0900

    Added atcoder modules

---
+++ atcoder.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+typedef long long ll;
+typedef modint998244353 mint;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    
+
+    return 0;
+}
