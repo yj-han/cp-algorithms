f54023ffe83ac2e7c08adc08bb873d0aef67baa7
Yeji Han <yejicse@gmail.com>
2023-07-17 22:07:08 +0900

    Implemented suffix array: LCP using Kasai's algorithm

--- string-processing/suffix_array.cpp
+++ string-processing/suffix_array.cpp
@@ -1,81 +1,91 @@
 #include <bits/stdc++.h>
-
 using namespace std;
- 
-vector<int> sort_cyclic_shifts(string const& s) {
-    int n = s.size();
-    const int alphabet = 256;
 
-    vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
-    for (int i = 0; i < n; i++)
-        cnt[s[i]]++;
-    for (int i = 1; i < alphabet; i++)
-        cnt[i] += cnt[i-1];
-    for (int i = 0; i < n; i++)
-        p[--cnt[s[i]]] = i;
-    c[p[0]] = 0;
-    int classes = 1;
-    for (int i = 1; i < n; i++) {
-        if (s[p[i]] != s[p[i-1]])
-            classes++;
-        c[p[i]] = classes - 1;
+class SuffixArray {
+private:
+    string s;
+    vector<int> suffix, rank, temp_rank, lcp;
+    int sz;
+
+    void make_suffix_array();
+    void make_lcp();
+    void update() { 
+        make_suffix_array();
+        make_lcp(); 
     }
 
-    vector<int> pn(n), cn(n);
-    for (int h = 0; (1 << h) < n; ++h) {
-        for (int i = 0; i < n; i++) {
-            pn[i] = p[i] - (1 << h);
-            if (pn[i] < 0)
-                pn[i] += n;
-        }
-        fill(cnt.begin(), cnt.begin() + classes, 0);
-        for (int i = 0; i < n; i++)
-            cnt[c[pn[i]]]++;
-        for (int i = 1; i < classes; i++)
-            cnt[i] += cnt[i-1];
-        for (int i = n-1; i >= 0; i--)
-            p[--cnt[c[pn[i]]]] = pn[i];
-        cn[p[0]] = 0;
-        classes = 1;
-        for (int i = 1; i < n; i++) {
-            pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
-            pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
-            if (cur != prev)
-                ++classes;
-            cn[p[i]] = classes - 1;
-        }
-        c.swap(cn);
+public:
+    SuffixArray(string& s) : s(s), sz(s.size()) { 
+        update(); 
     }
-    return p;
+
+    void change_string(string& s) { 
+        s = s; 
+        sz = s.size();
+        update(); 
+    }
+
+    vector<int> get_suffix_array() { 
+        return suffix; 
+    }
+
+    vector<int> get_lcp() { 
+        return lcp; 
+    }
+};
+
+void SuffixArray::make_suffix_array() {
+    suffix.assign(sz, 0);
+    rank.assign(sz + 1, -1);
+    temp_rank.assign(sz + 1, -1);
+
+    // ex) String = "banana" / Suffix = {0, 1, 2, 3, 4, 5}, Rank = {98, 97, 110, 97, 110, 97, -1}
+    for (int i = 0; i < sz; i++) {
+        suffix[i] = i;
+        rank[i] = s[i];
+    }
+
+    for (int compare_length = 1; compare_length < sz; compare_length <<= 1) {
+        auto cmp = [&](int x, int y) {
+            if (rank[x] == rank[y])
+                return rank[x + compare_length] < rank[y + compare_length];
+            return rank[x] < rank[y];
+        };
+
+        sort(suffix.begin(), suffix.end(), cmp);
+        temp_rank[suffix[0]] = 0;
+
+        for (int i = 1; i < sz; i++) {
+            if (cmp(suffix[i - 1], suffix[i]))
+                temp_rank[suffix[i]] = temp_rank[suffix[i - 1]] + 1;
+            else
+                temp_rank[suffix[i]] = temp_rank[suffix[i - 1]];
+        }
+        rank = temp_rank;
+    }
 }
 
-vector<int> suffix_array_construction(string s) {
-    s += "$";
-    vector<int> sorted_shifts = sort_cyclic_shifts(s);
-    sorted_shifts.erase(sorted_shifts.begin());
-    return sorted_shifts;
-}
+void SuffixArray::make_lcp() {
+    lcp.assign(sz, 0);
+    rank.assign(sz, 0);
 
-vector<int> lcp_construction(string const& s, vector<int> const& p) {
-    int n = s.size();
-    vector<int> rank(n, 0);
-    for (int i = 0; i < n; i++) {
-        rank[p[i]] = i;
-    }
+    for (int i = 0; i < sz; i++)
+        rank[suffix[i]] = i;
 
-    int k = 0;
-    vector<int> lcp(n-1, 0);
-    for (int i = 0; i < n; i++) {
-        if (rank[i] == n - 1) {
-            k = 0;
-            continue;
+    int len = 0;
+    for (int i = 0; i < sz; i++) {
+        int curr_rank = rank[i];
+
+        if (curr_rank > 0) {
+            int j = suffix[curr_rank - 1];
+
+            while (i+len < sz && j+len < sz && s[i+len] == s[j+len])
+                len++;
+            lcp[curr_rank] = len;
+
+            if (len) {
+                len--;
+            }
         }
-        int j = p[rank[i] + 1];
-        while (i + k < n && j + k < n && s[i+k] == s[j+k])
-            k++;
-        lcp[rank[i]] = k;
-        if (k)
-            k--;
     }
-    return lcp;
 }
