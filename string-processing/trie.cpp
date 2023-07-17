b0a92dd56d6410485af73203787dc15d5775760c
Yeji Han <yejicse@gmail.com>
2023-07-17 18:07:38 +0900

    Added string matching algorithms: trie, rabin-karp

--- string-processing/trie.cpp
+++ string-processing/trie.cpp
@@ -2,27 +2,37 @@
 using namespace std;
 
 const int K = 26;
-struct Vertex {
-    int next[K];
-    bool output = false;
 
-    Vertex() {
-        fill(begin(next), end(next), -1);
+struct Trie {
+    Trie* t[K];
+    bool end;
+
+    Trie() {
+        memset(t, 0, sizeof(t));
+        end = false;
+    }
+
+    ~Trie() {
+        for (int i = 0; i < K; i++) {
+            if (t[i]) delete t[i];
+        }
+    }
+
+    void insert(string& s, int idx) {
+        if (idx == s.length()) {
+            end = true;
+            return;
+        }
+        if (!t[s[idx]-'a']) {
+            t[s[idx]-'a'] = new Trie();
+        }
+        t[s[idx]-'a']->insert(s, idx+1);
+    }
+
+    bool solve(string& s, int idx) {
+        if (idx == s.length()) return false;
+        if (!t[s[idx]-'a']) return false;
+        if (t[s[idx]-'a']->end) return true;
+        return t[s[idx]-'a']->solve(s, idx+1);
     }
 };
-
-vector<Vertex> trie(1);
-
-void add_string(string const &s) {
-    int v = 0;
-
-    for (char ch : s) {
-        int c = ch - 'a';
-        if (trie[v].next[c] == -1) {
-            trie[v].next[c] = trie.size();
-            trie.emplace_back();
-        }
-        v = trie[v].next[c];
-    }
-    trie[v].output = true;
-}
